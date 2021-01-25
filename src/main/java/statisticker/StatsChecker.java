4058a9891048d8c821d745cced5b372c6b6e933b
Parthasaradhi <parthasaradhi.karnam@in.bosch.com>
2021-01-25 18:44:32 +0530

    Fixed junit issue

--- src/main/java/statisticker/StatsChecker.java
+++ src/main/java/statisticker/StatsChecker.java
@@ -4,25 +4,23 @@ import java.util.List;
 
 import alertservice.AlertService;
 
+
+
 public class StatsChecker {
-
-	private float maxThreshold;
-	private AlertService[] alerters;
-	private Statistics statistics;
-
-	public StatsChecker(float maxThreshold, AlertService[] alerters, Statistics statistics) {
-		this.maxThreshold = maxThreshold;
-		this.alerters = alerters;
-		this.statistics = statistics;
-	}
-
-	public void checkAndAlert(List<Float> numbers) {
-		float max = statistics.getMax(numbers);
-		if (!Float.isNaN(max) &&  max > this.maxThreshold) {
-			for (AlertService alerter : this.alerters) {
-				alerter.alert();
-			}
-		}
-	}
+  private float maxThreshold;
+  private AlertService[] alerters;
+  public StatsChecker(float maxThreshold, AlertService[] alerters) {
+    this.maxThreshold = maxThreshold;
+    this.alerters = alerters;
+  }
+  
+  public void checkAndAlert(List<Float> numbers) {
+    Statistics.Stats s = Statistics.getStatistics(numbers);
+    if(s.max != Float.NaN && s.max > maxThreshold && alerters.length > 0) {
+        for(AlertService alerter :  alerters) {
+          alerter.alert();
+        }
+    }
+  }
 
 }
